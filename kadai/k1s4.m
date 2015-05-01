1cc075d9a4b507987a1446969a219a9a2d0f5ef0
Kouei YAMAOKA <s1311403@abelia20.coins.tsukuba.ac.jp>
2015-05-01 16:17:02 +0900

    kadai1 end

--- kadai/k1s4.m
+++ kadai/k1s4.m
@@ -1,6 +1,15 @@
 k1s3
 
 yl = length(y);
-Z = zeros(2, yl);
-k = 1:yl;
-Z(:,k) = inv(W) * diag(y(:,k));
+Z = zeros(2, 2, yl);
+for k = 1:yl;
+    Z(:,:,k) = inv(W) * diag(y(:,k));
+end
+z1 = zeros(1,yl);
+z2 = zeros(1,yl);
+z3 = zeros(1,yl);
+z4 = zeros(1,yl);
+z1(1,:) = Z(1,1,:);
+z2(1,:) = Z(2,1,:);  % y1 -> z1 = z2
+z3(1,:) = Z(1,2,:);
+z4(1,:) = Z(1,2,:);  % y2 -> z2 = z4
