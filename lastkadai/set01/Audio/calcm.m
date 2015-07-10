7ec4977a3a8f2b39c6c5c5d2b8c9fa970c7a036f
KoueiYamaoka <kouei525@gmail.com>
2015-07-10 17:33:39 +0900

    デバッグするも解決せず

--- lastkadai/set01/Audio/calcm.m
+++ lastkadai/set01/Audio/calcm.m
@@ -4,7 +4,6 @@ mu = 0.05;
 ganma = 0.1;
 ty1 = ganma * conj(Y1);
 ty2 = ganma * conj(Y2);
-
 tmpY = [sum((Y1.*ty1)./normY1), sum((Y1.*ty2)./normY1); ...
         sum((Y2.*ty1)./normY2), sum((Y2.*ty2)./normY2)];
 tmpY = eye(2) + mu * (eye(2) - (1/fi) * tmpY);
--- lastkadai/set01/Audio/k221.m
+++ lastkadai/set01/Audio/k221.m
@@ -1,4 +1,4 @@
-clear all
+rm  clear all
 
 % 混合音声の読み込み-----------------------------
 [Mic1, Fs] = wavread('Mic_Ch1.wav');
@@ -10,14 +10,14 @@ s2 = Mic2';
 % IVAの実装
 % stft分析
 % s1,s2:混合信号, y1,y2:分離信号
-o = 2; % オーバーラップ幅
-p = 2^(15); % フレーム長
+o = 1/2; % オーバーラップ幅
+p = 2^(10); % フレーム長
 w = 1; % 窓関数、今はハミング
 sl = length(s1);
 
 % stft
-[X1, countX1] = stft(s1, 1/o, p, w);
-[X2, countX2] = stft(s2, 1/o, p, w);
+[X1, countX1] = stft(s1, o, p, w);
+[X2, countX2] = stft(s2, o, p, w);
 [fi,fs] = size(X1);
 
 % 分離行列Wの生成-------------------------------------------------------
@@ -27,7 +27,7 @@ L2 = ceil(fs/2);         % 総フレーム数 / 2
 rng(509);                % seed値
 W = rand(2,2,L2) - 0.5 + (rand(2,2,L2) - 0.5)*j;        % 分離行列
 compW = zeros(size(W));  % 反復終了判定の比較用
-maxloop = 3;          % 最大反復回数10000回
+maxloop = 300;          % 最大反復回数10000回
 
 % X, Yについて
 X = zeros(2,fi,L2); % 2行, 周波集インデックス列, フレーム数
@@ -38,6 +38,9 @@ X2 = reshape(X2,1,fi,L2);
 X(1,:,:) = X1;
 X(2,:,:) = X2;
 Y = zeros(size(X));
+tmp = zeros(size(W));
+XX = zeros(2,2,L2); % ループのmを先に計算したもの
+
 % 準備完了-----------------------------------------
 
 % warning
@@ -47,34 +50,36 @@ Y = zeros(size(X));
 
 % 反復計算開始-------------------------------------
 for k=1:maxloop
-    % Y = W * X 普通より速いと思うけどなぁ
-    tmpY = zeros(2,2,L2);
+    k
+    % Y = W * X 普通より0.2sec程度速い
+    % 間違っていないことを確認
     for l = 1:fi
         Y(:,l,:) = sum(W .* repmat(permute(X(:,l,:),[2,1,3]), 2, 1), 2);
     end
-    %終了----------------------------------------------------
+    %Yの計算終了----------------------------------------------------
 
     % Wの更新------------------------------------------------
+    % ここもあっていることを確認
     % 準備
     Y1 = squeeze(Y(1,:,:)).';
     Y2 = squeeze(Y(2,:,:)).';
     normY1 = sqrt(sum(abs(Y1).^2));
     normY2 = sqrt(sum(abs(Y2).^2));
-    XX = zeros(2,2,L2); % ループのmを先に計算したもの
     
     % 本体
+    % 普通に計算した場合より6秒ぐらい速い
     for i = 1:L2
         XX(:,:,i) = calcm(Y1(i,:), Y2(i,:), normY1, normY2, fi);
     end
     compW = W; % 反復終了判定用にWを保存
+
     % W = XX * W
     W(:,1,:) = sum(XX .* repmat(permute(W(:,1,:),[2,1,3]), 2,1), 2); 
     W(:,2,:) = sum(XX .* repmat(permute(W(:,2,:),[2,1,3]), 2,1), 2); 
     W(:,:,100) % 確認のため適当なWを表示
-    
-    % 終了----------------------------------------------------
+
+    % Wの更新終了----------------------------------------------------
     % 反復終了判定. tは収束したWの要素の数
-    tmp = zeros(size(W));
     tmp = abs(compW - W);
     t = numel(find(tmp < 10e-4))
     if  t == 4 * L2
@@ -88,10 +93,11 @@ for l = 1:fi
 end
 
 % istft
+% 半分にした周波数を復元する
 y1 = [squeeze(Y(1,:,:))];
 y2 = [squeeze(Y(2,:,:))];
 y1 = [y1, conj(fliplr(y1))];
-y2 = [y1, conj(fliplr(y2))];
-y1 = istft(squeeze(Y(1,:,:)), 1, fs, sl, countX1);
-y2 = istft(squeeze(Y(2,:,:)), 1, fs, sl, countX2);
+y2 = [y2, conj(fliplr(y2))];
+y1 = istft(y1, o, fs, sl, countX1);
+y2 = istft(y2, o, fs, sl, countX2);
     
